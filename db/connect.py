d1338277f5d41347d858a1b80f3c3fde7ca840eb
Paul Dhanda <paulvir.dhanda@weltel.org>
2019-12-11 16:26:50 -0800

    made db always available

--- db/connect.py
+++ db/connect.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 class DBConnection:
     def __init__(self, locationOfDb):
@@ -15,20 +16,24 @@ class DBConnection:
         """
         return 
 
-    def insertInstrumentData(self, name, source, timeframe, listOfInsertableData):
-        return 
-
-    def listTables(self):
+    def insertInstrumentData(self, name, source, timeframe, typeOfValue, locationOfData):
+        listOfDataPoints = self.csvToList(locationOfData)
+        newList = []
         cursor = self.connection.cursor()
+        self.createTable(name, timeframe)
         try:
-            cursor.execute('SELECT name from sqlite_master where type= "table"')
-            return cursor.fetchall()
+            for row in listOfDataPoints:
+                newTuple = (row[0], source, typeOfValue, row[1], row[2], row[3], row[4], row[5])
+                newList.append(newTuple)
+
+            cursor.executemany("""INSERT INTO "{0}_{1}" values (?,?,?,?,?,?,?,?)""".format(timeframe, name), newList)
+            self.connection.commit()
         except Exception as e:
             print(str(e))
         finally:
             cursor.close()
 
-    def createTable(self):
+    def listTables(self):
         cursor = self.connection.cursor()
         try:
             cursor.execute('SELECT name from sqlite_master where type= "table"')
@@ -42,15 +47,26 @@ class DBConnection:
         cursor = self.connection.cursor()
         try:
             strSQL = """CREATE TABLE IF NOT EXISTS "{0}_{1}" (
-                        local_time text NOT NULL UNIQUE,
+                        local_time text NOT NULL,
                         source text NOT NULL,
+                        type test NOT NULL,
                         open real NOT NULL,
                         high real NOT NULL,
                         low real NOT NULL,
                         close real NOT NULL,
-                        volume real NOT NULL)""".format(timeframe, name)
+                        volume real NOT NULL,
+                        PRIMARY KEY (local_time, source, type))""".format(timeframe, name)
             cursor.execute(strSQL)
         except Exception as e:
             print(str(e))
         finally:
             cursor.close()
+
+    def csvToList(self, locationOfCSV):
+        try:
+            with open(locationOfCSV, 'r') as f:
+                reader = csv.reader(f)
+                your_list = list(reader)
+            return your_list
+        except Exception as e:
+            print(str(e))
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from db.connect import DBConnection
 
 def main():
     db = DBConnection("./db/forex.db")
-    db.createTable('USDJPY', '1H')
+    db.insertInstrumentData('USDJPY', 'Dukascopy','1H', 'ASK', './csv/USDJPY/1H/USDJPY_Candlestick_1_Hour_ASK_31.12.2004-06.12.2019.csv')
+    db.insertInstrumentData('USDJPY', 'Dukascopy','1H', 'BID', './csv/USDJPY/1H/USDJPY_Candlestick_1_Hour_BID_31.12.2004-06.12.2019.csv')
+    
 
 if __name__ == "__main__":
     main()
